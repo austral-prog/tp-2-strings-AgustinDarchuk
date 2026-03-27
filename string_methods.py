53bc61cd4d04a418dc73422bf516f6357c52610b
AgustinDarchuk <adarchuk@mail.austral.edu.ar>
2026-03-27 11:33:44 -0300

    Add files via upload

--- string_methods.py
+++ string_methods.py
@@ -17,7 +17,7 @@ def string_methods():
     print(f"Find: {frase.find("gran")}")
     print(f"Replace: {frase.replace("programacion", "desarrollo")}")
     print(f"Count: {frase.count("a")}")
-    print(f"Contiene Python: {"Python" in frase}")
+    print(f"Contiene Python: {'Python' in frase}")
     print(f"Contiene Java: {"Java" in frase}")
     print(f"Slice: {frase[0:6]}")
     print(f"Paso: {frase[0:6:2]}")
